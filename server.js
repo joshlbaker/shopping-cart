7815ff31cbdb30e4861f17e60054f254fc0aff3f
Josh Baker <joshualbaker111@gmail.com>
2015-11-15 14:47:41 -0800

    first routes finished

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ var app = express();
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
-
+// Homepage route
+app.get('/',function(req,res){
+    res.sendFile('main.html',{'root':__dirname + '/public'});
+})
 
 
 
